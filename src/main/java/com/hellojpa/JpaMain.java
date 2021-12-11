6f5b8e153e552d32ff2bade246b8c67b7d02504b
isemang <a55306039@gmail.com>
2021-12-11 22:09:36 +0900

    feat - Member 객체 추가, 수정, 삭제 + JPQL 기초 학습

--- src/main/java/com/hellojpa/JpaMain.java
+++ src/main/java/com/hellojpa/JpaMain.java
@@ -4,25 +4,78 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+        //db 당 하나만 생성
         EntityManagerFactory enf = Persistence.createEntityManagerFactory("hello");
+        //고객의 요청이 올 때마다 계속 썼다가, 버렸다가 함
+        //그래서 엔티티매니저는 쓰레드간에 절대 절대 공유하면 안됨!!!
+        //JPA의 모드 데이터 변경은 트랜잭션 안에서 실행해야 함
         EntityManager em = enf.createEntityManager();
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("helloA");
+        /* 1. member 추가 */
+//        try {
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("helloB");
+//
+//            em.persist(member);
+//
+//            tx.commit();
+//        } catch (Exception e) {
+//            tx.rollback();
+//        } finally {
+//            em.close();
+//        }
 
-        em.persist(member);
+        /* 2. member 수정 */
+//        try {
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJPA");
+//
+//            tx.commit();
+//        } catch (Exception e) {
+//            tx.rollback();
+//        } finally {
+//            em.close();
+//        }
 
-        tx.commit();
+        /* 3. member 삭제 */
+//        try {
+//            Member findMember = em.find(Member.class, 1L);
+//
+//            em.remove(findMember);
+//
+//            tx.commit();
+//        } catch (Exception e) {
+//            tx.rollback();
+//        } finally {
+//            em.close();
+//        }
 
-        //실제 동작하는 코드 작성하는 곳
-        em.close();
+        /* 4. JPQL 이용 */
+        //JPQL : 쿼리를 짜는 거긴 하지만, SQL처럼 RDB 종속적인 쿼리가 아니라 "객체"에 종속적인 쿼리를 짤 수 있음
+        //또한 ORACLE에서 My Sql로 바꿔도 전~혀 코드상에서 바꿀 필요가 없음
+        try {
+            List<Member> result = em.createQuery("select m from Member as m")
+                    .setFirstResult(1)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
 
         enf.close();
     }
