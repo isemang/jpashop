7d60b49103264063febb8d5940e4d0fc05b94b8f
isemang <a55306039@gmail.com>
2021-12-20 23:35:08 +0900

    feat - 다대다관계 학습: 다대다를 풀어주는 MemberProduct entity 생성

--- src/main/java/com/hellojpa/Member.java
+++ src/main/java/com/hellojpa/Member.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -31,6 +33,9 @@ public class Member {
     @JoinColumn(name = "TEAM_ID")
     //하나의 팀에 여러 사람이 소속됨 -> manyToOne(member입장에선 many, team입장에서는 one)
     private Team team;  //Team과 Member의 관계가 일대다, 다대일 관계인지 알려줘야 함
+
+    @OneToMany(mappedBy = "product")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     //연간관계 편의 메서드 사용!!
     public void changeTeam(Team team) {
